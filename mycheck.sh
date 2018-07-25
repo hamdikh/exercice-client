c06fc5be251e3bd049170d59221312fcb5164e05
Hamdi KHELIL <khelilhamdi@gmail.com>
2018-07-25 21:16:30 +0200

    Cleaning + Adding usage

--- mycheck.sh
+++ mycheck.sh
@@ -1,21 +1,34 @@
-#!/bin/bash                                                                                                 
-                                                                                                            
-                                                                                                            
-FILE="$1"                                                                                          
-                                                                                                            
-                                                                                                            
-ELEMENTS_COUNT=$(xmllint --xpath 'count(//checks/ping/node()/text())' $FILE)                                
-echo $ELEMENTS_COUNT                                   
+    #!/bin/bash                                                                                                 
 
-for ( var=1; var<=$ELEMENTS_COUNT; var++ ))                                                               
-do                                                                                                          
-    SITE_NAME=$(xmllint --xpath "name(//checks/ping/*[$var])" $FILE )                                                              
-    SITE=$(xmllint --xpath "string(//checks/ping/*[$var]/text())" $FILE | cut -f 1 -d ":")                          
-    ping -c1 $SITE 1>/dev/null 2>/dev/null                                                                           
-    if [ $? -eq 0 ]                                                                                         
-    then                                                                                                    
-    echo "$SITE_NAME OK"
-    else
-    echo "$SITE_NAME KO"                                                                              
-    fi                                                                                                                                                                                                             
-done                                                                                               
+    # Dependencies libxml2-utils     
+    # To install them just use apt-get install libxml2-utils / yum install libxml2-utils 
+
+    ##
+    ## Usage Function
+    ##
+    usage(){ 
+        echo "Usage $0 sourcefile"
+        exit 1
+    } 
+
+    # Pas de paramètre 
+    [[ $# -lt 1 ]] && usage
+                                                                                                                
+    FILE="$1"                                                                                          
+                                                                                                                
+
+    # Count elements under ping node                                                                                                    
+    ELEMENTS_COUNT=$(xmllint --xpath 'count(//checks/ping/node()/text())' $FILE)                                
+
+    for (( var=1; var<=$ELEMENTS_COUNT; var++ ))                                                               
+    do                                                                                                          
+        SITE_NAME=$(xmllint --xpath "name(//checks/ping/*[$var])" $FILE )                                                              
+        SITE=$(xmllint --xpath "string(//checks/ping/*[$var]/text())" $FILE | cut -f 1 -d ":")                          
+        ping -c1 $SITE 1>/dev/null 2>/dev/null                                                                           
+        if [ $? -eq 0 ]                                                                                         
+        then                                                                                                    
+            echo "$SITE_NAME OK"
+        else
+            echo "$SITE_NAME KO"                                                                              
+        fi                                                                                                                                                                                                             
+    done                                                                                               
